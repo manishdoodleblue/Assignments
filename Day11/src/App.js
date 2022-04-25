3a6c69e28acbe4e33073eba226be377ef4c085a2
manishdoodleblue <manishdoodleblue@gmail.com>
2022-04-25 16:02:16 +0530

    day11

---
+++ Day11/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Sidebar from './Sidebar'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Sidebar/>
+      </div>
+    )
+  }
+}
+
+export default App
